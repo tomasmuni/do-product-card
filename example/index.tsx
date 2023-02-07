b556adf012d462bb632943d2fc87b16eb9422f18
tommun <tomas.munoz@idrica.com>
2023-02-07 13:53:27 +0100

    Prueba cambio version

--- example/index.tsx
+++ example/index.tsx
@@ -6,7 +6,7 @@ import { ProductCard,ProductImage,ProductTitle,ProductButtons } from '../.';
 const App = () => {
   const product = {
     id: '1',
-    title: 'Coffee Mug - Card',
+    title: 'Coffee Mug - Card !!!',
     img: './coffee-mug.png'
   }
   
